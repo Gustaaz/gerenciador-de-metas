c0b14edef238edcf1ba85a12efd7250263728bb0
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-10 13:58:46 -0400

    acao de criacao da meta

---
+++ backend/src/http/controllers/goals/create-goal.ts
@@ -0,0 +1,27 @@
+import { db } from '@/db'
+import { goals } from '@/db/schema'
+
+interface CreateGoalRequest {
+  title: string
+  desiredWeekFrequency: number
+}
+
+type CreateGoalResponse = {
+  id: string
+  title: string
+  desiredWeekFrequency: number
+  createdAt: Date
+}
+
+export async function createGoal({
+  title,
+  desiredWeekFrequency,
+}: CreateGoalRequest): Promise<{ goal: CreateGoalResponse }> {
+  const result = await db
+    .insert(goals)
+    .values({ title, desiredWeekFrequency })
+    .returning()
+  return {
+    goal: result[0],
+  }
+}
