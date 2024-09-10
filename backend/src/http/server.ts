aa2005a9119576049e440facb80a651e089780a3
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-10 13:58:25 -0400

    configuracao do provaider fastify-type-provider-zod

--- backend/src/http/server.ts
+++ backend/src/http/server.ts
@@ -1,6 +1,17 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { goalsRoutes } from './controllers/goals/routes'
 
-const app = fastify()
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(goalsRoutes)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
