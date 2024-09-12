675d60b770730f08e6719093ace1dae6e0fdf96f
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-12 14:19:12 -0400

    funcionalidade de criacao de meta

--- frontend/src/components/create-goal.tsx
+++ frontend/src/components/create-goal.tsx
@@ -13,55 +13,114 @@ import {
   RadioGroupItem,
 } from './ui/radio-group'
 import { Button } from './ui/button'
+import { Controller, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useQueryClient } from '@tanstack/react-query'
+import { z } from 'zod'
+import { createGoal } from '../http/create-goal'
 
+const createGoalForm = z.object({
+  title: z.string().min(1, 'Informe a atividade que deseja realizar'),
+  desiredWeekFrequency: z.coerce.number().min(1).max(7),
+})
+
+type CreateGoalForm = z.infer<typeof createGoalForm>
+
+const options = [
+  { value: '1', icon: '🥱' },
+  { value: '2', icon: '🙂' },
+  { value: '3', icon: '😎' },
+  { value: '4', icon: '😜' },
+  { value: '5', icon: '🤯' },
+  { value: '6', icon: '🔥' },
+]
 export function CreateGoal() {
-  const options = [
-    { value: '1', icon: '🥱' },
-    { value: '2', icon: '🙂' },
-    { value: '3', icon: '😎' },
-    { value: '4', icon: '😜' },
-    { value: '5', icon: '🤯' },
-    { value: '6', icon: '🔥' },
-  ]
+  const queryClient = useQueryClient()
+
+  const { register, control, handleSubmit, formState, reset } =
+    useForm<CreateGoalForm>({
+      resolver: zodResolver(createGoalForm),
+    })
+
+  async function handleCreateGoal({
+    desiredWeekFrequency,
+    title,
+  }: CreateGoalForm) {
+    await createGoal({
+      title,
+      desiredWeekFrequency,
+    })
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+
+    reset()
+  }
 
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
         <div className="flex flex-col gap-3">
           <div className="flex items-center justify-between">
-            <DialogTitle>Criar meta</DialogTitle>
+            <DialogTitle>Cadastrar meta</DialogTitle>
             <DialogClose>
               <X className="size-5 text-zinc-600" />
             </DialogClose>
           </div>
+
           <DialogDescription>
             Adicione atividades que te fazem bem e que você quer continuar
             praticando toda semana.
           </DialogDescription>
         </div>
-        <form className="flex flex-1 flex-col justify-between">
+
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex-1 flex flex-col justify-between"
+        >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Qual a atividade?</Label>
               <Input
                 id="title"
                 autoFocus
-                placeholder="Praticar, meditar, etc..."
+                placeholder="Praticar exercícios, meditar, etc..."
+                {...register('title')}
               />
+
+              {formState.errors.title && (
+                <p className="text-red-400 text-sm">
+                  {formState.errors.title.message}
+                </p>
+              )}
             </div>
             <div className="flex flex-col gap-2">
-              <Label>Quantas vezes na semana?</Label>
-              <RadioGroup>
-                {options.map(option => (
-                  <RadioGroupItem key={option.value} value={option.value}>
-                    <RadioGroupIndicator />
-                    <span>{option.value}x por semana</span>
-                    <span>{option.icon}</span>
-                  </RadioGroupItem>
-                ))}
-              </RadioGroup>
+              <Label htmlFor="title">Quantas vezes na semana?</Label>
+              <Controller
+                control={control}
+                name="desiredWeekFrequency"
+                defaultValue={1}
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                      defaultValue="1"
+                    >
+                      {options.map(option => (
+                        <RadioGroupItem key={option.value} value={option.value}>
+                          <RadioGroupIndicator />
+                          <span>{option.value}x por semana</span>
+                          <span>{option.icon}</span>
+                        </RadioGroupItem>
+                      ))}
+                    </RadioGroup>
+                  )
+                }}
+              />
             </div>
           </div>
+
           <div className="flex items-center gap-3">
             <DialogClose asChild>
               <Button type="button" className="flex-1" variant="secondary">
