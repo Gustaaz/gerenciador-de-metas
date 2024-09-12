8513f2d236a8c0716433e701a417b550f470852c
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-12 10:16:09 -0400

    tela de empty de metas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,21 @@
+import logo from './assets/logo-in-orbit.svg'
+import leftStart from './assets/left-start.svg'
+import { Plus } from 'lucide-react'
 export function App() {
-  return <h1 className="text-3xl font-bold">Hello word</h1>
+  return (
+    <div className="h-screen flex flex-col justify-center items-center gap-8 ">
+      <img src={logo} alt="in.orbit" />
+      <img src={leftStart} alt="in.orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
+  )
 }
