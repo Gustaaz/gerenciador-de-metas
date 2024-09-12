9a487d50b4d03b4f5093215dd57ab14d20e2fed0
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-12 09:25:41 -0400

    registrando o plugino do fastify/cors

--- backend/src/http/server.ts
+++ backend/src/http/server.ts
@@ -7,8 +7,14 @@ import {
 import { getPendingRoute } from './controllers/goals/routes/get-pending-route'
 import { createGoalRoute } from './controllers/goals/routes/create-goal-route'
 import { createGoalCompletionRoute } from './controllers/goals/routes/create-goal-completion-route'
+import { getWeekSummaryRoute } from './controllers/goals/routes/get-week-summary-route'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
@@ -16,6 +22,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(getPendingRoute)
 app.register(createGoalRoute)
 app.register(createGoalCompletionRoute)
+app.register(getWeekSummaryRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running on http://localhost:3333')
