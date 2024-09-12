24cbfe5fec3dde44908100ec35a0882cfd2a0c32
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-12 13:36:46 -0400

    funcionalidade de completar meta

--- frontend/src/components/pending-goals.tsx
+++ frontend/src/components/pending-goals.tsx
@@ -1,9 +1,11 @@
 import { Plus } from 'lucide-react'
 import { OutlineButton } from './ui/outline-button'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { gertPendingGoals } from '../http/get-pending-goals'
+import { createGoalCompletion } from '../http/create-goal-completion'
 
 export function PendingGoals() {
+  const queryClient = useQueryClient()
   const { data } = useQuery({
     queryKey: ['pending-goals'],
     queryFn: gertPendingGoals,
@@ -14,12 +16,20 @@ export function PendingGoals() {
     return null
   }
 
+  async function handleCreateGoalCompletion(goalId: string) {
+    await createGoalCompletion(goalId)
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+  }
+
   return (
     <div className="flex flex-wrap gap-3">
       {data.map(goal => (
         <OutlineButton
           key={goal.id}
           disabled={goal.completionCount >= goal.desiredWeekFrequency}
+          onClick={() => handleCreateGoalCompletion(goal.id)}
         >
           <Plus className="size-4 text-zinc-400" />
           {goal.title}
