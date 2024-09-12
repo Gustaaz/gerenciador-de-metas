1c56312920652df1ecffdf82adbdae5faeb1cb9d
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-09-12 10:29:55 -0400

    alterando o button html para button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import logo from './assets/logo-in-orbit.svg'
 import leftStart from './assets/left-start.svg'
 import { Plus } from 'lucide-react'
+import { Button } from './components/ui/button'
 export function App() {
   return (
     <div className="h-screen flex flex-col justify-center items-center gap-8 ">
@@ -9,13 +10,10 @@ export function App() {
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
+      <Button>
         <Plus className="size-4" />
         Cadastrar meta
-      </button>
+      </Button>
     </div>
   )
 }
